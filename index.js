9608c79e6be2906047f1e75d1947141c3bcf08a8
mekryzen01 <631463026@crru.ac.th>
2023-01-22 20:07:31 +0700

    22/1/66

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + './index.html');
 });
 
 const users = {};
